086c048732529aefc335b2c1a2209b4f7816c884
Ershad Ahamed Sultan Alaudeen <ershadahamed89@gmail.com>
2024-09-29 02:22:40 +0800

    Just htmls

---
+++ script.js
@@ -0,0 +1,8 @@
+function scrollToServices() {
+    window.scrollTo({
+        top: document.querySelector('main').offsetTop,
+        behavior: 'smooth'
+    });
+}
+
+console.log("Welcome to Asian Electric Services!");
